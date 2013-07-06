ce3442e3e2249dc7dbbee1f54561336882bcb6d5
Aleksander Dąbrowski <tjeden@gmail.com>
2013-07-06 11:28:48 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlassTest::Application.config.secret_token = 'd3a0b6d0e814f29b7659133c70d761d0722a80c50ab7190d251c2a495e118589d2e222439fa401a9aae4851f133c0b74811b27104ebf2bffbbd49d29dc9d6c73'
